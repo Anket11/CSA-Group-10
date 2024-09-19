4b96bd069dcdec5c15527b063c4ed064fb92ff7f
Anket <patilanket11@gmail.com>
2024-09-18 20:15:34 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        Assembler assembler = new Assembler();
+
+        // Specify the source file and the output files
+        String sourceFile = "input.txt";
+        String loadFile = "load.txt";
+        String listingFile = "listing.txt";
+        // Assemble the source code and generate the output files
+        assembler.readSourceFile(sourceFile);
+        assembler.firstPass();
+        assembler.secondPass();
+        assembler.writeFiles(listingFile, loadFile);
+
+        System.out.println("Assembly completed. Listing and load files generated.");
+    }
+}
